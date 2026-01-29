40be6c89378eeeb072441783f68d1d3608cc945c
Chenfeng Li <49363621+Chenfeng-Li@users.noreply.github.com>
2026-01-29 17:57:58 -0600

    Update script to allow label only video

--- video_prediction.py
+++ video_prediction.py
@@ -12,7 +12,7 @@ matplotlib.use("Agg")
 from tools import plot_images_labels
 from image_prediction import load_model, predict_image
 
-def predict_video(model, video, output):
+def predict_video(model, video, save, combine=False):
     """
     For a video, predict frame by frame and output the video.
     """
@@ -24,7 +24,7 @@ def predict_video(model, video, output):
     h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-    writer = cv2.VideoWriter(output, fourcc, out_fps, (w, h), True)
+    writer = cv2.VideoWriter(save, fourcc, out_fps, (w, h), True)
     frame_idx = 0
 
     try:
@@ -36,7 +36,7 @@ def predict_video(model, video, output):
 
                 frame_rgb = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2RGB)
                 pred = predict_image(model, frame_rgb)
-                overlay_rgb = plot_images_labels(frame_rgb, pred, combine=True, return_array=True)
+                overlay_rgb = plot_images_labels(frame_rgb, pred, combine, return_array=True) if combine else plot_images_labels(None, pred, combine, return_array=True)
                 if overlay_rgb.dtype != np.uint8:
                     overlay_rgb = np.clip(overlay_rgb, 0, 255).astype(np.uint8)
 
@@ -52,12 +52,14 @@ def predict_video(model, video, output):
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--video', type=str, default="")
-    parser.add_argument('--output', type=str, default="predict.mp4")
+    parser.add_argument('--combine', type=str, default="False")
+    parser.add_argument('--save', type=str, default="predict.mp4")
     parser.add_argument('--model', type=str, default="checkpoints/best.pt")
 
     args = parser.parse_args()
     video = args.video
-    output = args.output
+    combine = args.combine in ["True", "true", "TRUE"]
+    save = args.save
     model_path = args.model
 
     if not os.path.isfile(video):
@@ -65,10 +67,4 @@ if __name__=="__main__":
         sys.exit()
     
     model, _ = load_model(model_path)
-    predict_video(model, video, output)
-
-    
-
-
-    
-
+    predict_video(model, video, save, combine)
