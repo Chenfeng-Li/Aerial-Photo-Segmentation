680233d04ecf538324c215c3c4c23352720e62ad
Chenfeng-Li <jmlichenfeng@gmail.com>
2026-02-03 23:04:32 -0600

    Fix download function

--- checkpoints/pretrained_model.py
+++ checkpoints/pretrained_model.py
@@ -1,7 +1,10 @@
 from huggingface_hub import hf_hub_download
+from pathlib import Path
+
+path = Path(__file__).resolve().parent
 hf_hub_download(
     repo_id="Chenfeng-Li/aerial-photo-segmentation",
     filename="best.pt",
-    local_dir_use_symlinks=False
+    local_dir=path
 )
 print("Pretrained checkpoint best.pt downloaded.")
